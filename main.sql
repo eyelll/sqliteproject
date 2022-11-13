eebfb96cf1611dbd294fae99ed6f6c44c649f603
eyelll <eyelll422@gmail.com>
2022-11-13 12:27:31 +0000

    organized the code

--- main.sql
+++ main.sql
@@ -1,10 +1,30 @@
 
-create table product(brand char(15), ProductType char(15),ProductSize char(20),CategoryName char(15),CategoryID int, ProductAvailable char(5), ProductID int, ProductName char(25), primary key (ProductID));
+create table product(brand char(15),
+ProductType char(15),
+ProductSize char(20),
+CategoryName char(15),
+CategoryID int, 
+ProductAvailable char(5), 
+ProductID int, 
+ProductName char(25),
+primary key (ProductID));
   
 
-create table orders(OrderedItems int, TotalPrice int, OrderDate char(20),ProductID int, OrderID int, primary key(OrderID), foreign key(ProductID) references product(ProductID)
+create table orders(OrderedItems int,
+TotalPrice int,
+OrderDate char(20),
+ProductID int,
+OrderID int,
+primary key(OrderID), foreign key(ProductID) references product(ProductID)
   
   on delete cascade
   on update cascade);
 
-create table customer(ID int, Name char(15), City Char(20), PhoneNumber char(12), Email char(30), Street char(15), OrderID int, primary key(ID), Foreign key(orderID) references orders(orderID));
+create table customer(ID int,
+Name char(15),
+City Char(20),
+PhoneNumber char(12),
+Email char(30),
+Street char(15),
+OrderID int,
+primary key(ID), Foreign key(orderID) references orders(orderID));
