3574700ac5df71ecb462808c4e89acdc2111d753
eyelll <eyelll422@gmail.com>
2022-11-13 14:15:36 +0200

    Update main.sql

--- main.sql
+++ main.sql
@@ -5,6 +5,6 @@ create table product(brand char(15), ProductType char(15),ProductSize char(20),C
 create table orders(OrderedItems int, TotalPrice int, OrderDate char(20),ProductID int, OrderID int, primary key(OrderID), foreign key(ProductID) references product(ProductID)
   
   on delete cascade
-  on update cascade);
+  on update cascade );
 
 create table customer(ID int, Name char(15), City Char(20), PhoneNumber char(12), Email char(30), Street char(15), OrderID int, primary key(ID), Foreign key(orderID) references orders(orderID));
